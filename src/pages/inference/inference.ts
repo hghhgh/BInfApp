07e885876172145789447f1691765072b92ab349
ghiassi <hosein.ghiasy@gmail.com>
2018-11-02 20:20:57 +0330

    using dm4js

--- src/pages/inference/inference.ts
+++ src/pages/inference/inference.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Storage} from "@ionic/storage";
+import {findProbability4Bayes, inferBayesian} from 'dm4js'
 
 /**
  * Generated class for the InferencePage page.
@@ -76,14 +77,14 @@ export class InferencePage {
                   // console.log('P_Hp',P_Hp);
                   // console.log('P_Ep_Hp',P_Ep_Hp);
                   // console.log('P_En_Hn',P_En_Hn);
-                  let prs = this.findProbabilities(P_Hp, P_Ep_Hp, P_En_Hn);
+                  let prs = findProbability4Bayes(P_Hp, P_Ep_Hp, P_En_Hn);
                   // console.log(prs);
-                  this.P_Hp_Tp = this.inferBayesian(prs);
+                  this.P_Hp_Tp = inferBayesian(prs);
                 });
               }
               else {
-                let prs = this.findProbabilities(this.newprior, P_Ep_Hp, P_En_Hn);
-                this.P_Hp_Tp = this.inferBayesian(prs);
+                let prs = findProbability4Bayes(this.newprior, P_Ep_Hp, P_En_Hn);
+                this.P_Hp_Tp = inferBayesian(prs);
               }
             }
             else {
@@ -140,60 +141,6 @@ export class InferencePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InferencePage');
-  }
-
-  /**
-   * Baysian inference functions
-   * target is P(Hypothesis | Evidence) = P(H and E) / P(E)
-   * H+(or Hp) means the target is sick
-   * H-(or Hn) means the target is healthy
-   * E+(or Ep) means the Evidence is occured
-   * E-(or En) means the Evidence isn't occured
-   *
-   * P_Hp = .0; // from our knowledge or litrature
-   * P_Ep_Hp = .0; // from specification of the test that provides the evidence
-   * P_En_Hn = .0; // from specification of the test that provides the evidence
-   */
-  findProbabilities(P_Hp, P_Ep_Hp, P_En_Hn) {
-    //  if P(H+)
-    //  P_Hp
-    //  P_Ep_Hp
-    let P_En_Hp = 1 - P_Ep_Hp;
-
-    // if P(H-)
-    let P_Hn = 1.0 - P_Hp;
-    //  P_Tn_Hn
-    let P_Ep_Hn = 1 - P_En_Hn;
-
-    let res = {
-      P_Hp: P_Hp,
-      P_Ep_Hp: P_Ep_Hp,
-      P_En_Hp: P_En_Hp,
-      P_Hn: P_Hn,
-      P_Ep_Hn: P_Ep_Hn,
-      P_En_Hn: P_En_Hn
-    };
-
-    return res;
-
-  }
-
-  /**
-   * Infer based on Bayes rule
-   *
-   * return : the probability of being sick given positive evidence
-   */
-  inferBayesian(probs) {
-    let P_HpEp = probs.P_Hp * probs.P_Ep_Hp;
-    let P_HnEp = probs.P_Hn * probs.P_Ep_Hn;
-
-    let denom = P_HpEp + P_HnEp;
-
-    console.log('P_HpEp', P_HpEp);
-    console.log('P_HnEp', P_HnEp);
-    console.log('denom', denom);
-    // P_Hp_Ep
-    return P_HpEp / denom;
   }
 
 }
